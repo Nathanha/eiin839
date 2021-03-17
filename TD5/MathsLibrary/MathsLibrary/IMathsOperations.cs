aa172ef5b5c32ca447c3523c4705e1e0bf418ddf
Nathan <haro.nathan@gmail.com>
2021-03-17 15:38:35 +0100

    TD5 REST

--- TD5/MathsLibrary/MathsLibrary/IMathsOperations.cs
+++ TD5/MathsLibrary/MathsLibrary/IMathsOperations.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 
 namespace MathsLibrary
@@ -11,15 +12,19 @@ namespace MathsLibrary
     public interface IMathsOperations
     {
         [OperationContract]
+        [WebInvoke(Method = "GET",RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "Add?x={x}&y={y}")]
         int Add(int x, int y);
 
         [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "Multiply?x={x}&y={y}")]
         int Multiply(int x, int y);
 
         [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "Subtract?x={x}&y={y}")]
         int Subtract(int x, int y);
 
         [OperationContract]
+        [WebInvoke(Method = "GET", RequestFormat = WebMessageFormat.Json, BodyStyle = WebMessageBodyStyle.Wrapped, UriTemplate = "Divide?x={x}&y={y}")]
         double Divide(double x, double y);
     }
 }
